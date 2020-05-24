bb763c7b3a77632c36ed817a80483e7493fab216
Mohit Choudhary <choudhary18111@iiitd.ac.in>
2020-05-25 02:50:41 +0530

    added mordered and changed ui

--- Code/ml_pipeline/model/BaseMLJob.py
+++ Code/ml_pipeline/model/BaseMLJob.py
@@ -1,11 +1,18 @@
+from email import utils
+
 import ManageJob as manage_job
 from MLJobConfig import MLJobConfig
 
+# import ml_pipeline.utils.Logging as logging
+# logger = logging.get_job_logger("/home/mohit/all_jobs/20200523015453985876/.config/logs")
 
 class BaseMLJob:
 
     def __init__(self, job_id):
         print("Inside BaseMLJob initailize")
+        # logger.debug("Inside BaseMLJob debug")
+        # logger.info("Inside BaseMLJob initailize info")
+        # logger.error("Inside BaseMLJob initailize error")
         self.job_id = job_id
         self.job_data = None
         self.config = MLJobConfig()
